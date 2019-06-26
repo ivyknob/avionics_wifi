0348b3efa581a49722ba4f182178f86fee672cde
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2019-06-26 17:12:40 +0300

    Change toast-message js-lib

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 /* global global */
-// Заменить на yarn add toastify-js
-import toastr from 'toastr'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'toastr/build/toastr.css'
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 import '@ivyknob/avionics_js'
 import RModal from 'rmodal'
 import modalTemplate from './modal.html'
 
-global.toastr = toastr;
+global.Toastify = Toastify;
 global.modalTemplate = modalTemplate;
 global.RModal = RModal;
